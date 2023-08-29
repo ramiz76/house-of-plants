3b26f924588faccfdef6edfa3099e5c39e5077cd
angelikavilde <angelikavilde@gmail.com>
2023-08-29 20:30:31 +0100

    email tests added and comma tests:

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -31,9 +31,40 @@ def test_remove_duplicate_plants_data_saved(duplicate_data):
     assert returned_data.iloc[0].equals(assumed_return)
     assert returned_data.iloc[1:3].empty
 
-def test_remove_comma():
-    """Verifying that comma removal was correct"""
+
+def test_remove_comma_no_comma():
+    """Verifies that text is kept the same if without a comma"""
     text = "Text without comma"
     assert remove_comma(text) == text
-    text += ","
-    assert remove_comma(text) == text
+
+
+def test_remove_comma_comma_exists():
+    """Verifying that comma removal was correct"""
+    text = "Text with a comma,"
+    assert remove_comma(text) == text[:-1]
+
+
+def test_remove_formatting():
+    """Test that verifies correct formatting on 'scientific_name' column"""
+    formatted_text = "['Some text added']"
+    not_formatted_text = "Some text added"
+    assert remove_formatting(formatted_text) == not_formatted_text
+    assert remove_formatting(not_formatted_text) == not_formatted_text
+
+
+def test_find_email_correct():
+    """Verifies correct email is found"""
+    potential_email = "test.email@yahoo.com"
+    assert find_email(potential_email) == potential_email
+
+
+def test_find_email_incorrect():
+    """Verifies email is not found in incorrect formatted email"""
+    potential_email = "+++++@yahoo.com"
+    assert find_email(potential_email) is None
+
+
+def test_find_email_wront_type():
+    """Verifies find_email function returns None
+    with wrong type of value entered"""
+    assert find_email([]) is None
