1225155cec7fd2b7ab46d6f7a9552ef138f9c8d1
angelikavilde <angelikavilde@gmail.com>
2023-08-29 20:02:35 +0100

    docstrings added to functions and file and test for a comma removing function is added

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -1,4 +1,4 @@
-""""""
+"""Tests for transform.py file"""
 
 from pytest import fixture
 import pandas as pd
@@ -13,6 +13,7 @@ from transform import verifying_botanist_data, find_phone_number, find_email
 
 @fixture
 def duplicate_data() -> DataFrame:
+    """Returns a data-frame with duplicate data"""
     return pd.DataFrame(data=[[1,1],[1,2],[1,3]],
         columns=["plant_name","test1"], index=[1,2,3])
 
@@ -29,3 +30,10 @@ def test_remove_duplicate_plants_data_saved(duplicate_data):
     assumed_return = duplicate_data.iloc[0]
     assert returned_data.iloc[0].equals(assumed_return)
     assert returned_data.iloc[1:3].empty
+
+def test_remove_comma():
+    """Verifying that comma removal was correct"""
+    text = "Text without comma"
+    assert remove_comma(text) == text
+    text += ","
+    assert remove_comma(text) == text
