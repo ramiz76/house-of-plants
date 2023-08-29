487e8eddedd3eca01c52e6f637307afc0e067640
ramiz <ramizalipir76@gmail.com>
2023-08-29 17:49:02 +0100

    create terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "DATABASE_NAME" {
+  type = string
+
+}
+
+variable "DATABASE_USERNAME" {
+  type = string
+
+}
+
+variable "DATABASE_ENDPOINT" {
+  type = string
+
+}
+
+variable "DATABASE_PASSWORD" {
+  type = string
+
+}
+
