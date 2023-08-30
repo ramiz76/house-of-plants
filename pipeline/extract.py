d95d9822d199e4c5f37aafa1066f653bea1bd585
Lishein <trainee.lishein.wmsigmalabs.co.uk>
2023-08-30 10:17:32 +0100

    formatting

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -11,14 +11,11 @@ def get_plant_data_by_id(plant_id: int) -> dict:
      a dict of all data for the plant."""
 
     url = f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}"
-
     try:
         response = requests.get(url, timeout=5).json()
-
     except requests.exceptions.Timeout:
         response = {
             "error": "Timeout: The request could not be completed.", "plant_id": plant_id}
-
     return response
 
 
@@ -40,11 +37,9 @@ def obtain_relevant_data(plant: dict) -> dict:
             "botanist_name": plant["botanist"]["name"],
             "email": plant["botanist"]["email"], "phone": plant["botanist"]["phone"]
         }
-
     except:
         relevant_data = {
             "api_id": plant["plant_id"], "error": "Missing field in data."}
-
     return relevant_data
 
 
@@ -53,32 +48,21 @@ def get_relevant_plant_data() -> list[dict]:
     and returns list."""
 
     list_of_plants = []
-
     for i in range(51):
-
         plant = get_plant_data_by_id(i)
-
         if "error" not in plant.keys():
-
             if plant["temperature"] is None:
-
                 relevant_data = {
                     "api_id": plant["plant_id"], "error": "Missing temperature reading."}
-
             elif plant["soil_moisture"] is None:
-
                 relevant_data = {
                     "api_id": plant["plant_id"], "error": "Missing soil_moisture reading."}
-
             else:
                 relevant_data = obtain_relevant_data(plant)
-
         else:
             relevant_data = {
                 "api_id": plant["plant_id"], "error": plant["error"]}
-
         list_of_plants.append(relevant_data)
-
     return list_of_plants
 
 
@@ -86,7 +70,6 @@ def create_download_folders() -> None:
     """Creates a folder with the name 'extracted_data' if it doesn't already exist."""
 
     folder_exists = os.path.exists('extracted_data')
-
     if not folder_exists:
         os.makedirs('extracted_data')
 
@@ -96,7 +79,6 @@ def add_to_csv(list_of_plants: list[dict]):
 
     dataframe = pd.DataFrame(list_of_plants)
     csv_filename = "extracted_data/plant_data.csv"
-
     dataframe.to_csv(csv_filename, index=False)
 
 
