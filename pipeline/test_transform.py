af51380bf5737ea665c604b9dd48df7446dc4cb6
angelikavilde <angelikavilde@gmail.com>
2023-08-30 11:03:25 +0100

    added tests for numeric data and time-string data

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -2,8 +2,8 @@
 
 from datetime import datetime
 
-from pytest import fixture
-from pandas import DataFrame
+import pandas as pd
+from pytest import fixture, raises
 
 from transform import remove_duplicate_plants, remove_comma
 from transform import remove_formatting, removing_invalid_values
@@ -13,20 +13,23 @@ from transform import verifying_botanist_data, find_phone_number, find_email
 
 
 @fixture
-def duplicate_data() -> DataFrame:
+def duplicate_data() -> pd.DataFrame:
     """Returns a data-frame with duplicate data"""
-    return DataFrame(data=[[1,1],[1,2],[1,3]],
-        columns=["plant_name","test1"], index=[1,2,3])
+
+    return pd.DataFrame(data=[["1", 1], ["1", 2], ["1", 3]], 
+        columns=["plant_name", "test1"], index=[1, 2, 3])
 
 
 def test_remove_duplicate_plants_data_removed(duplicate_data):
     """Verifies that correct amount of data was removed"""
+
     returned_data = remove_duplicate_plants(duplicate_data)
-    assert returned_data.shape == (1,2)
+    assert returned_data.shape == (1, 2)
 
 
 def test_remove_duplicate_plants_data_saved(duplicate_data):
     """Verifies that correct data was saved"""
+
     returned_data = remove_duplicate_plants(duplicate_data)
     assumed_return = duplicate_data.iloc[0]
     assert returned_data.iloc[0].equals(assumed_return)
@@ -35,18 +38,21 @@ def test_remove_duplicate_plants_data_saved(duplicate_data):
 
 def test_remove_comma_no_comma():
     """Verifies that text is kept the same if without a comma"""
+
     text = "Text without comma"
     assert remove_comma(text) == text
 
 
 def test_remove_comma_comma_exists():
     """Verifying that comma removal was correct"""
+
     text = "Text with a comma,"
     assert remove_comma(text) == text[:-1]
 
 
 def test_remove_formatting():
     """Test that verifies correct formatting on 'scientific_name' column"""
+
     formatted_text = "['Some text added']"
     not_formatted_text = "Some text added"
     assert remove_formatting(formatted_text) == not_formatted_text
@@ -55,12 +61,14 @@ def test_remove_formatting():
 
 def test_find_email_correct():
     """Verifies correct email is found"""
+
     potential_email = "test.email@yahoo.com"
     assert find_email(potential_email) == potential_email
 
 
 def test_find_email_incorrect():
     """Verifies email is not found in incorrectly formatted email"""
+
     potential_email = "+++++@yahoo.com"
     assert find_email(potential_email) is None
 
@@ -68,18 +76,21 @@ def test_find_email_incorrect():
 def test_find_email_wront_type():
     """Verifies find_email function returns None
     with wrong type of value entered"""
+
     assert find_email([]) is None
 
 
 def test_time_format_changed_incorrect():
     """Verifies None is returned when time-string is
     in incorrect format"""
+
     assert time_format_changed(None) is None
 
 
 def test_time_format_changed_correct():
     """Verifies datetype value is returned when time-string
     is in correct format"""
+
     time_string = "Tue, 29 Aug 2023 13:24:30 GMT"
     assert isinstance(time_format_changed(time_string), datetime)
 
@@ -87,6 +98,7 @@ def test_time_format_changed_correct():
 def test_find_phone_number_correct():
     """Verifies None is returned when incorrectly
     formatted phone number is entered"""
+
     number = "I am a fake phone number"
     assert find_phone_number(number) is None
 
@@ -94,5 +106,66 @@ def test_find_phone_number_correct():
 def test_find_phone_number_incorrect():
     """Verifies that find_phone_number correctly
     identifies an example phone-number"""
+
     number = "001-630-832-2711x5822"
     assert find_phone_number(number) == number
+
+
+def test_change_to_numeric_fully_numeric_df(duplicate_data):
+    """Verifies that numeric data is kept"""
+
+    returned_data = change_to_numeric(duplicate_data, ["plant_name"])
+    assert not returned_data.iloc[0:3].empty
+
+
+def test_change_to_numeric_not_fully_numeric_df(duplicate_data):
+    """Verifies that non numeric data is removed"""
+
+    extra_row = pd.DataFrame(["not numeric", 2], index=[4, 5])
+    duplicate_data = pd.concat([extra_row, duplicate_data])
+    returned_data = change_to_numeric(duplicate_data, ["plant_name"])
+
+    with raises(IndexError):
+        returned_data.iloc[3]
+
+
+def test_missing_time_fixed_time():
+    """Verifies datetime is changed from a string"""
+
+    time = missing_time_fixed("2023-05-26 12:01:34", {})
+    assert isinstance(time, datetime)
+
+
+def test_missing_time_fixed_not_time():
+    """Verifies that None is correctly kept
+    as None"""
+
+    time = missing_time_fixed(None, {})
+    assert not isinstance(time, datetime)
+
+
+@fixture
+def time_df() -> pd.DataFrame:
+    """Returns a testing data-frame with time-strings columns"""
+
+    data = [[f"2023-05-26 12:0{i}:34", f"Tue, 29 Jan 2023 13:2{i}:30 GMT", None] for i in range (5)]
+    data.append([None, None, "error found"])
+    return pd.DataFrame(data=data, 
+        columns=["recording_taken", "last_watered", "error"], index=[1, 2, 3, 4, 5, 6])
+
+
+def test_missing_time_fixed_wrong_type(time_df):
+    """Verifying that recording_taken is adjusted with last value if None"""
+
+    cache_dict = {}
+    returned_data = time_df["recording_taken"].apply(lambda row: missing_time_fixed(row, cache_dict))
+    assert isinstance(returned_data.iloc[-1], datetime)
+
+
+def test_correct_time_recorded(time_df):
+    """Verifying that time is correctly changed"""
+
+    returned_data, error_data = correct_time_recorded(time_df)
+    assert error_data.shape == (1, 3)
+    assert returned_data.shape == (5, 3)
+    assert all(isinstance(returned_data["last_watered"].iloc[i], datetime) for i in range (5))
