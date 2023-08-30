e61424f57418a43e2f3c4f317b51e1cae1915480
ramiz <ramizalipir76@gmail.com>
2023-08-30 13:25:22 +0100

    removed ec2 script and replaced with ecs

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_ecr_repository" "house-of-plants-ecr" {
+  name = "house-of-plants-ecr"
+}
 
 resource "aws_security_group" "house-of-plants-rds-sg" {
   name        = "house-of-plants-rds-sg"
@@ -41,19 +44,105 @@ resource "aws_db_instance" "house-of-plants-rds" {
   db_name                = var.DATABASE_NAME
 }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+# resource "aws_ecs_cluster" "house-of-plants-cluster" {
+#   name = "house-of-plants-cluster"
+# }
 
-  name = "house-of-plants-ec2"
 
-  instance_type          = "t2.micro"
-  key_name               = "radl_key_pair"
-  monitoring             = true
-  vpc_security_group_ids = [resource.aws_security_group.house-of-plants-rds-sg.id]
-  subnet_id              = "subnet-"
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
+
+resource "aws_security_group" "house-of-plants-ecs-sg" {
+  name   = "house-of-plants-ecs-sg"
+  vpc_id = "vpc-0e0f897ec7ddc230d"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_iam_role" "house-of-plants-ecs-execution-role"{
+    name = "house-of-plants-ecs-execution-role"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+        {
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Sid = ""
+            Principal = {
+                Service = "ecs.amazonaws.com"
+            }
+        },
+    ]
+    })
+
+}
+
+# resource "aws_ecs_task_definition" "house-of-plants-pipeline-ecs"{
+#     family = "house-of-plants-pipeline-ecs"
+#     cpu = "1024"
+#     network_mode = "awsvpc"
+#     memory = "3072"
+#     requires_compatibilities = ["FARGATE"]
+#     execution_role_arn = aws_iam_role.house-of-plants-ecs-execution-role.arn
+
+#     container_definitions= jsonencode([
+#         {
+#             "name": "to be determined",
+#             "image": "to be determined",
+#             "portMappings": [
+#                 {
+#                     "name": "443-mapping",
+#                     "cpu":0,
+#                     "containerPort": 443,
+#                     "hostPort": 443,
+#                     "protocol": "tcp",
+#                     "appProtocol": "https"
+#                 },
+#                 {
+#                     "name": "8501-mapping",
+#                     "containerPort": 8501,
+#                     "hostPort": 8501,
+#                     "protocol": "tcp",
+#                     "appProtocol": "http"
+#                 }
+#             ],
+#             "essential": true,
+#             "environment": [
+#                 {
+#                     "name": "DATABASE_NAME",
+#                     "value": var.DATABASE_NAME
+#                 },
+#                 {
+#                     "name": "DATABASE_USERNAME",
+#                     "value": var.DATABASE_USERNAME
+#                 },
+#                 {
+#                     "name": "DATABASE_ENDPOINT",
+#                     "value": aws_db_instance.house-of-plants-rds.endpoint
+#                 },
+#                 {
+#                     "name": "DATABASE_PASSWORD",
+#                     "value": var.DATABASE_PASSWORD
+#                 }],
+#             "logConfiguration": {
+#                 "logDriver": "awslogs",
+#                 "options": {
+#                     "awslogs-create-group": "true",
+#                     "awslogs-group": "/ecs/",
+#                     "awslogs-region": "eu-west-2",
+#                     "awslogs-stream-prefix": "ecs"
+#                 }
+#             }
+#         }
+#     ])
+# }
