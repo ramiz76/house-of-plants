388074b4206e91cf4a4200fb71222ad8cda61a57
Lishein <trainee.lishein.wmsigmalabs.co.uk>
2023-08-30 13:37:58 +0100

    multiprocessing for extract

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -49,7 +49,7 @@ def correct_time_recorded(plant_data: pd.DataFrame) -> tuple[pd.DataFrame]:
     plant_data["last_watered"] = plant_data["last_watered"].apply(
         time_format_changed)
     plant_data["recording_taken"] = plant_data["recording_taken"].apply(lambda
-                                row: missing_time_fixed(row, cache_dict))
+                                                                        row: missing_time_fixed(row, cache_dict))
     plant_errors = plant_data[plant_data["error"].notnull()]
     plant_data = plant_data[~plant_data["error"].notnull()]
     plant_data = plant_data[plant_data["recording_taken"]
@@ -122,7 +122,7 @@ def find_phone_number(row: str) -> str | None:
 
     number_expression = r"(\+?\(?[0-9-\.]+\)?(?:[x0-9-\.]+)?)"
     match = fullmatch(number_expression, row)
-    return match.group().replace(".","-") if match is not None else None
+    return match.group().replace(".", "-") if match is not None else None
 
 
 def verifying_botanist_data(plant_data: pd.DataFrame) -> pd.DataFrame:
@@ -140,7 +140,7 @@ def transform_script() -> None:
     time_started = datetime.now()
     print("Transforming...")
 
-    csv_filename = "extracted_data/plant_data.csv"
+    csv_filename = "data/plant_data.csv"
     data = pd.read_csv(csv_filename)
     data.set_index("api_id")
     data, error_rows = correct_time_recorded(data)
@@ -152,5 +152,6 @@ def transform_script() -> None:
     data.to_csv(csv_filename)
 
     time_finished = datetime.now()
-    print(f"Total transformation time: {time_finished - time_started} seconds.")
-transform_script()
+    print(
+        f"Total transformation time: {time_finished - time_started} seconds.")
+
