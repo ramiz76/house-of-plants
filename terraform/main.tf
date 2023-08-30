1429800b9fbe1c6760cf4b5b15ea38f443fa044e
ramiz <ramizalipir76@gmail.com>
2023-08-30 16:19:08 +0100

    need to test ecs and accesses

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_ecr_repository" "house-of-plants-ecr" {
-  name = "house-of-plants-ecr"
+resource "aws_ecr_repository" "house-of-plants-short-term-ecr" {
+  name = "house-of-plants-short-term-ecr"
 }
+
+resource "aws_ecr_repository" "house-of-plants-long-term-ecr" {
+  name = "house-of-plants-long-term-ecr"
+}
+
 
 resource "aws_security_group" "house-of-plants-rds-sg" {
   name        = "house-of-plants-rds-sg"
@@ -28,10 +33,26 @@ resource "aws_security_group" "house-of-plants-rds-sg" {
 
 }
 
-resource "aws_db_instance" "house-of-plants-rds" {
+resource "aws_db_instance" "house-of-plants-short-term-rds" {
 
   instance_class         = "db.t3.micro"
-  identifier             = "house-of-plants-rds"
+  identifier             = "house-of-plants-short-term-rds"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  username               = var.DATABASE_USERNAME
+  password               = var.DATABASE_PASSWORD
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  db_subnet_group_name   = "public_subnet_group"
+  vpc_security_group_ids = [resource.aws_security_group.house-of-plants-rds-sg.id]
+  availability_zone      = "eu-west-2a"
+  db_name                = var.DATABASE_NAME
+}
+
+resource "aws_db_instance" "house-of-plants-long-term-rds" {
+
+  instance_class         = "db.t3.micro"
+  identifier             = "house-of-plants-long-term-rds"
   allocated_storage      = 20
   engine                 = "postgres"
   username               = var.DATABASE_USERNAME
@@ -51,8 +72,8 @@ resource "aws_db_instance" "house-of-plants-rds" {
 
 
 
-resource "aws_security_group" "house-of-plants-ecs-sg" {
-  name   = "house-of-plants-ecs-sg"
+resource "aws_security_group" "house-of-plants-ecs-api-sg" {
+  name   = "house-of-plants-ecs-api-sg"
   vpc_id = "vpc-0e0f897ec7ddc230d"
   ingress {
     from_port   = 443
@@ -69,26 +90,48 @@ resource "aws_security_group" "house-of-plants-ecs-sg" {
   }
 }
 
-resource "aws_iam_role" "house-of-plants-ecs-execution-role"{
-    name = "house-of-plants-ecs-execution-role"
-    assume_role_policy = jsonencode({
+resource "aws_iam_role" "house-of-plants-ecs-execution-role" {
+  name = "house-of-plants-ecs-execution-role"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-        {
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Sid = ""
-            Principal = {
-                Service = "ecs.amazonaws.com"
-            }
-        },
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+      },
     ]
+  })
+  inline_policy {
+    name = "ecs-task-inline-policy"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action   = "ecs:DescribeTaskDefinition",
+          Effect   = "Allow",
+          Resource = "*"
+        },
+        {
+          Action   = "ecs:DescribeTasks",
+          Effect   = "Allow",
+          Resource = "*"
+        },
+        {
+          Action   = "ecs:RunTask",
+          Effect   = "Allow",
+          Resource = "*"
+        }
+      ]
     })
-
+  }
 }
 
-# resource "aws_ecs_task_definition" "house-of-plants-pipeline-ecs"{
-#     family = "house-of-plants-pipeline-ecs"
+# resource "aws_ecs_task_definition" "house-of-plants-short-pipeline-ecs"{
+#     family = "house-of-plants-short-pipeline-ecs"
 #     cpu = "1024"
 #     network_mode = "awsvpc"
 #     memory = "3072"
@@ -128,7 +171,7 @@ resource "aws_iam_role" "house-of-plants-ecs-execution-role"{
 #                 },
 #                 {
 #                     "name": "DATABASE_ENDPOINT",
-#                     "value": aws_db_instance.house-of-plants-rds.endpoint
+#                     "value": aws_db_instance.house-of-plants-short-term-rds.endpoint
 #                 },
 #                 {
 #                     "name": "DATABASE_PASSWORD",
@@ -146,3 +189,161 @@ resource "aws_iam_role" "house-of-plants-ecs-execution-role"{
 #         }
 #     ])
 # }
+
+
+# resource "aws_ecs_task_definition" "house-of-plants-long-pipeline-ecs"{
+#     family = "house-of-plants-long-pipeline-ecs"
+#     cpu = "1024"
+#     network_mode = "awsvpc"
+#     memory = "3072"
+#     requires_compatibilities = ["FARGATE"]
+#     execution_role_arn = aws_iam_role.house-of-plants-ecs-execution-role.arn
+
+#     container_definitions= jsonencode([
+#         {
+#             "name": "to be determined",
+#             "image": "to be determined",
+#             "portMappings": [
+#                 {
+#                     "name": "443-mapping",
+#                     "cpu":0,
+#                     "containerPort": 443,
+#                     "hostPort": 443,
+#                     "protocol": "tcp",
+#                     "appProtocol": "https"
+#                 },
+#                 {
+#                     "name": "8501-mapping",
+#                     "containerPort": 8501,
+#                     "hostPort": 8501,
+#                     "protocol": "tcp",
+#                     "appProtocol": "http"
+#                 }
+#             ],
+#             "essential": true,
+#             "environment": [
+#                 {
+#                     "name": "DATABASE_NAME",
+#                     "value": var.DATABASE_NAME
+#                 },
+#                 {
+#                     "name": "DATABASE_USERNAME",
+#                     "value": var.DATABASE_USERNAME
+#                 },
+#                 {
+#                     "name": "DATABASE_ENDPOINT",
+#                     "value": aws_db_instance.house-of-plants-long-term-rds.endpoint
+#                 },
+#                 {
+#                     "name": "DATABASE_PASSWORD",
+#                     "value": var.DATABASE_PASSWORD
+#                 }],
+#             "logConfiguration": {
+#                 "logDriver": "awslogs",
+#                 "options": {
+#                     "awslogs-create-group": "true",
+#                     "awslogs-group": "/ecs/",
+#                     "awslogs-region": "eu-west-2",
+#                     "awslogs-stream-prefix": "ecs"
+#                 }
+#             }
+#         }
+#     ])
+# }
+
+
+
+
+rescourse "aws_scheduler_schedule" "house-of-plants-short-term-schedular" {
+    name = "house-of-plants-short-schedular"
+    group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "rate(1 minute)"
+
+  target {
+    arn      = var.ecs_task_definition_arn #This is the arn of the ecs 
+    role_arn = aws_iam_role.house-of-plants-ecs-execution-role.arn
+  }
+}
+
+rescourse "aws_scheduler_schedule" "house-of-plants-long-term-schedular" {
+    name = "house-of-plants-long-schedular"
+    group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "rate(6 hours)"
+
+  target {
+    arn      = var.ecs_task_definition_arn #This is the arn of the ecs 
+    role_arn = aws_iam_role.house-of-plants-ecs-execution-role.arn
+  }
+}
+
+
+resource "aws_ecs_task_definition" "house-of-plants-short-dashboard"{
+    family = "house-of-plants-short-dashboard"
+    cpu = "1024"
+    network_mode = "awsvpc"
+    memory = "3072"
+    requires_compatibilities = ["FARGATE"]
+    execution_role_arn = aws_iam_role.house-of-plants-ecs-execution-role.arn
+
+    container_definitions= jsonencode([
+        {
+            "name": "short-term-dashboard",
+            "image": "to be determined",
+            "portMappings": [
+                {
+                    "name": "short-term-dashboard-80-tcp",
+                    "cpu":0,
+                    "containerPort": 80,
+                    "hostPort": 80,
+                    "protocol": "tcp",
+                    "appProtocol": "http"
+                },
+                {
+                    "name": "short-term-dashboard-8501-tcp",
+                    "containerPort": 8501,
+                    "hostPort": 8501,
+                    "protocol": "tcp",
+                    "appProtocol": "http"
+                }
+            ],
+            "essential": true,
+            "environment": [
+                {
+                    "name": "DATABASE_NAME",
+                    "value": var.DATABASE_NAME
+                },
+                {
+                    "name": "DATABASE_USERNAME",
+                    "value": var.DATABASE_USERNAME
+                },
+                {
+                    "name": "DATABASE_ENDPOINT",
+                    "value": aws_db_instance.house-of-plants-short-term-rds.endpoint
+                },
+                {
+                    "name": "DATABASE_PASSWORD",
+                    "value": var.DATABASE_PASSWORD
+                }],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-create-group": "true",
+                    "awslogs-group": "/ecs/",
+                    "awslogs-region": "eu-west-2",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
+        }
+    ])
+}
+
