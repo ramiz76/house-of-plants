fa7679df4f3a81ac5ddd9ad5eb75c031fb099231
angelikavilde <angelikavilde@gmail.com>
2023-08-31 16:42:13 +0100

    added more tests for load

--- pipeline/test_load_short.py
+++ pipeline/test_load_short.py
@@ -19,7 +19,7 @@ def test_get_db_connection(monkeypatch):
 
 
 def test_add_plant_ids_to_sensor_df(fake_dataframe, fake_dataframe_2):
-    """"""
+    """Verifies that None values are kept when received for ids"""
 
     returned_df = add_plant_ids_to_sensor_df(FakeConn(), fake_dataframe, fake_dataframe)
     assert returned_df.equals(fake_dataframe_2)
@@ -32,10 +32,54 @@ def file_removed() -> None:
 
 
 def test_create_dataframe(monkeypatch, fake_dataframe):
-    """"""
+    """Verifies that a new dataframe is created from passed data"""
 
     monkeypatch.setattr("pandas.read_csv", lambda *args: fake_dataframe)
     monkeypatch.setattr("os.remove", lambda *args: file_removed)
 
     returned_df = create_dataframe("file")
     assert returned_df.equals(fake_dataframe)
+
+
+def test_insert_dataframe_into_origin_table(fake_dataframe, capfd):
+    """Verifies that execute is ran without errors"""
+
+    insert_dataframe_into_origin_table(FakeConn(), fake_dataframe)
+    captured = capfd.readouterr()
+
+    assert "Executed!" in captured.out
+
+
+def test_insert_dataframe_into_plant_table(fake_dataframe, capfd):
+    """Verifies that execute is ran without errors"""
+
+    insert_dataframe_into_origin_table(FakeConn(), fake_dataframe)
+    captured = capfd.readouterr()
+
+    assert "Executed!" in captured.out
+
+def test_insert_dataframe_into_sensor_result_table(fake_dataframe, capfd):
+    """Verifies that execute is ran without errors"""
+
+    insert_dataframe_into_sensor_result_table(FakeConn(), fake_dataframe)
+    captured = capfd.readouterr()
+
+    assert "Executed!" in captured.out
+
+
+def test_add_botanist_ids_to_sensor_df(fake_dataframe_2):
+    """Verifies that execution has changed the correct things in the dataframe"""
+
+    fake_dataframe_2 = fake_dataframe_2.rename(columns={"api_id": "botanist_id", "plant_id": "phone"})
+
+    returned_df = add_botanist_ids_to_sensor_df(FakeConn(), fake_dataframe_2, fake_dataframe_2)
+    assert returned_df["phone"].equals(fake_dataframe_2["phone"])
+
+
+def test_add_origin_ids_to_plant_df(monkeypatch, fake_dataframe_2):
+    """Verifies that execution has changed the correct things in the dataframe"""
+
+    fake_dataframe_2 = fake_dataframe_2.rename(columns={"api_id": "latitude", "plant_id": "longitude"})
+    monkeypatch.setattr(FakeConn.cursor.__enter__.fetchone, lambda **kwargs: {"origin_id": None})
+    returned_df = add_origin_ids_to_plant_df(FakeConn(), fake_dataframe_2, fake_dataframe_2)
+    assert returned_df["origin_id"].empty
