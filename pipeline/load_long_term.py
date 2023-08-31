c8d1f68608b150728c7372f7d3f04574426cb32a
Lishein <trainee.lishein.wmsigmalabs.co.uk>
2023-08-31 14:10:57 +0100

    long term script converting to csv added

--- pipeline/load_long_term.py
+++ pipeline/load_long_term.py
@@ -28,48 +28,43 @@ def get_short_term_db_connection(config: dict) -> psycopg2.extensions.connection
         raise psycopg2.DatabaseError("Error connecting to database.")
 
 
-def retrieve_data_older_than_24_hours(conn):
+def retrieve_data_older_than_24_hours(connection: psycopg2.extensions.connection) -> None:
+    """Retrieves data older than 24hrs from the short term RDS database and stores as a CSV file."""
 
-    twenty_four_hours_ago = datetime.now() - timedelta(hours=24)
+    twenty_four_hours_ago = datetime.now() - timedelta(hours=6)
+    formatted_time = twenty_four_hours_ago.strftime('%Y-%m-%d %H:%M:%S')
 
     query_for_data = f"""SELECT
-        p.plant_name,
-        p.scientific_name,
-        p.api_id,
-        p.cycle,
-        sr.last_watered,
-        sr.soil_moisture,
-        sr.temperature,
-        p.sunlight, 
-        sr.recording_taken,
-        o.longitude,
-        o.latitude,
-        o.country,
-        o.continent,
+        p.plant_name as plant_name,
+        p.scientific_name as scientific_name,
+        p.api_id as api_id,
+        p.cycle as cycle,
+        sr.last_watered as last_watered,
+        sr.soil_moisture as soil_moisture,
+        sr.temperature as temperature,
+        p.sunlight as sunlight, 
+        sr.recording_taken as recording_taken,
+        o.longitude as longitude,
+        o.latitude as latitude,
+        o.country as country,
+        o.continent as continent,
         b.name AS botanist_name,
-        b.email,
-        b.phone,
+        b.email as email,
+        b.phone as phone,
         av.type_of_availability AS error
         FROM sensor_result sr
         JOIN plant p ON sr.plant_id = p.plant_id
         JOIN origin o ON p.origin_id = o.origin_id
         JOIN botanist b ON sr.botanist_id = b.botanist_id
         JOIN plant_availability av ON sr.availability_id = av.availability_id
-        WHERE sr.recording_taken < {twenty_four_hours_ago}"""
+        WHERE sr.recording_taken < '{formatted_time}'"""
 
-    with conn.cursor() as curr:
+    with connection.cursor() as cur:
+        cur.execute(query_for_data)
+        result = cur.fetchall()
 
-def combine_csv_files():
-
-    main_csv_dateframe = pd.read_csv('original_data/plant_data.csv')
-    new_csv_dateframe = pd.read_csv('24hr_data/plant_data1.csv')
-
-    combined_df = pd.concat(
-        [main_csv_dateframe, new_csv_dateframe], ignore_index=True)
-
-    combined_df = combined_df.drop(columns=['Unnamed: 0'])
-
-    combined_df.to_csv('combined.csv', index=False)
+    data = pd.DataFrame(result)
+    data.to_csv("24_hour_data", index=False)
 
 
 if __name__ == "__main__":
@@ -83,3 +78,5 @@ if __name__ == "__main__":
     }
 
     short_db_conn = get_short_term_db_connection(config)
+
+    retrieve_data_older_than_24_hours(short_db_conn)
